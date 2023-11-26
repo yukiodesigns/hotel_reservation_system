affefa1a1ccd700bb50d2c75dc76976bf174adc5
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-11-27 01:36:13 +0300

    Configured tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,10 +3,23 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     screens: {
-      sm: '640px',
+      sm: '480px',
       md: '768px',
-      lg: '1024px',
-      xl: '1234px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
     },
     container: {
       padding: {
